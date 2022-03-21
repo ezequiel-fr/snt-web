25c735b1b82fff4f2c2ca277546468bc8bea273c
TheRedMine TheredMaths <theredminedu51@gmail.com>
2022-03-21 04:41:02 +0100

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,7 @@ import Carousel from './carousel.js';
 
 window.onload = () => {
     // Carousel(document.querySelector('section .carousel'));
+
+    const menuIcon = document.querySelector('.menu .icon');
+    menuIcon.addEventListener('click', () => menuIcon.classList.toggle('active'));
 };
