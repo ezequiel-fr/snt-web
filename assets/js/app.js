284aed1451a2c845cccefd9ebf688df5c9d99f86
TheRedMine TheredMaths <theredminedu51@gmail.com>
2022-03-21 09:07:30 +0100

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,5 +4,11 @@ window.onload = () => {
     // Carousel(document.querySelector('section .carousel'));
 
     const menuIcon = document.querySelector('header .icon-menu');
-    menuIcon.addEventListener('click', () => menuIcon.classList.toggle('active'));
+    menuIcon.addEventListener('click', () => {
+        menuIcon.classList.toggle('active');
+        
+        document.querySelector('header .second-part').classList[
+            menuIcon.classList.contains('active') ? 'add' : 'remove'
+        ]('active');
+    });
 };
