0f9b4dd3aaaf602dbad6ef744b39621692e1fb24
TheRedMine TheredMaths <theredminedu51@gmail.com>
2022-03-25 09:11:05 +0100

    Update

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -8,34 +8,43 @@ export default (carousel) => {
     carousel.innerHTML = "";
     carousel.classList.add('carouselController', 'carousel-main');
 
-    // style
-    const style = document.createElement('style');
-    // container
-    style.innerHTML = `.carousel.carouselController {width: 100%; height: 100%; background: #f00; display: flex; flex-direction: row;}`;
-    // arrows
-    style.innerHTML += `.carousel.carouselController .carouselController.arrow {margin: 5px 0; width: 36px; height: 14px; transform: rotate(90deg); cursor: pointer; flex: 1;} .carousel.carouselController .carouselController.arrow.left {transform: rotate(270deg)} .carousel.carouselController .carouselController.arrow span, .carousel.carouselController .carouselController.arrow span::before {content: ''; display: block; position: relative; top: 6px; left: 0; width: 20px; height: 2px; background: #666; border-radius: 1px; transform: rotate(-30deg);} .carousel.carouselController .carouselController.arrow span::before {height: 2.75px; top: 7.9px; left: 13.5px; transform: rotate(59deg); background: #666;}`;
-    // content
-    style.innerHTML += `.carousel.carouselController .carousel-body {flex: 2;}`;
-
     // add arrows
     const arrow = (name) => {
+        const box = document.createElement('div');
         const arrow = document.createElement('div');
 
+        arrow.classList.add('arrow', 'carouselController', name);
+        box.classList.add('arrow-box', 'carouselController', 'carousel-el');
+        
         arrow.appendChild(document.createElement('span'));
-        arrow.classList.add('arrow', 'carouselController', name, 'carousel-el');
+        box.appendChild(arrow);
 
-        return arrow;
+        return box;
     };
 
     carousel.appendChild(arrow('left'));
 
     // container
     const container = document.createElement('div');
-    container.classList.add('content', 'carouselController', 'carousel-body');
+          container.classList.add('container', 'carouselController', 'carousel-body');
     
+    const content = document.createElement('div'); // content
+          content.classList.add('container', 'carouselController', 'carousel-body');
+    
+    const index = document.createElement('div'); // index
+          index.classList.add('index', 'carouselController');    
+    
+    container.appendChild(content);
+    container.appendChild(index);
     carousel.appendChild(container);
 
     // last arrow
     carousel.appendChild(arrow('right'));
-    carousel.appendChild(style);
+
+    // style
+    const style = document.createElement('style');
+    style.innerHTML = `.carousel.carouselController {display: flex; flex-direction: row; flex-wrap: nowrap; width: 720px; height: 480px; margin: 5px; border: 1px solid #222; border-radius: 5px; overflow: hidden;} .carousel.carouselController .arrow-box.carouselController {flex: 1; display: flex; flex-direction: row; justify-content: center; align-items: center;} .carousel.carouselController .arrow.carouselController {display: block; cursor: pointer; width: 20px; height: 34px; transition: all .2s; z-index: 3;} .carousel.carouselController .arrow.carouselController.left {transform: rotate(.5turn)} .carousel.carouselController .arrow.carouselController:hover {transform: scale(125%)} .carousel.carouselController .arrow.carouselController.left:hover {transform: rotate(.5turn) scale(125%)} .carousel.carouselController .arrow.carouselController span, .arrow span::after {position: relative; top: 8px; display: block; width: 20px; height: 2px; background: #333; border-radius: 2px; transform: rotate(60deg); cursor: pointer;} .carousel.carouselController .arrow.carouselController span::after {content: ''; left: calc(50% + 4px);} .carousel.carouselController .container.carouselController, .container.carouselController .content.carouselController {flex: 100%; width: 100%; height: 100%;} .carousel.carouselController .container.carouselController .content.carouselController {display: flex; position: relative; flex-direction: column; justify-content: center; align-items: center; z-index: 1; width: calc(100% + 40px); right: 20px;} .carousel.carouselController .container.carouselController .index.carouselController {display: inline-flex; position: relative; bottom: 24px; height: 14px; width: 100%; justify-content: center; z-index: 2;} .carousel.carouselController .container.carouselController .index.carouselController span {display: block; width: 10px; height: 10px; background: transparent; border: 2px solid var(--primary-color); border-radius: 10px; margin: 0 2px;} .carousel.carouselController .container.carouselController .index.carouselController span.filled {background: var(--primary-color)}`;
+    
+    try { document.head.appendChild(style); }
+    catch { document.querySelector('body').appendChild(style); }
 };
